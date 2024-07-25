6ca8ac4fda3cfe0b256c9026cfe700aa371d1a6c
Aditya_Mishra <adityasaiyansuper@gmail.com>
2024-07-25 16:17:01 +0530

    add logout and get current user functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { UserContext, UserProvider } from "./contexts";
 import { Login, Logout, Profile, Navbar } from "./components/index";
@@ -16,9 +16,9 @@ function App() {
         },
         body: JSON.stringify({
           email: loginInfo.email,
-          password: loginInfo.password
+          password: loginInfo.password,
         }),
-        credentials: "include"
+        credentials: "include",
       });
 
       if (!res.ok) {
@@ -36,14 +36,57 @@ function App() {
       console.log("Login Failed!!!", error.message);
     }
   };
-  const logoutuser = () => {};
+  const logoutUser = async () => {
+    try {
+      const res = await fetch("/api/v1/users/logout", {
+        method: "POST",
+        credentials: "include",
+      });
+
+      if (!res.ok) {
+        console.log("Logout Error :: ", res);
+        throw new Error(`Logout ERROR :: ${res.status}`);
+      }
+
+      console.log("Logged Out successfully");
+      setUser({});
+      setIsLoggedIn(false);
+    } catch (error) {
+      console.log("Logout failed :: ", error.message);
+    }
+  };
   const updateUser = () => {};
 
   // useEffect -> to fetch details of currently logged in user
   // whenever this component reloads
+  useEffect(() => {
+    async function getCurrentUser() {
+      try {
+        const res = await fetch("/api/v1/users/current-user", {
+          method: "GET",
+          credentials: "include",
+        });
+
+        if (!res.ok) {
+          console.log("response not ok");
+          throw new Error(`Could not get currenr User :: ${res.status}`);
+        }
+
+        const result = await res.json();
+        
+        // here our user data is in result.data
+        setUser(result.data)
+        setIsLoggedIn(true)
+      } catch (error) {
+        console.log("Could not get current user :: ", error);
+      }
+    }
+
+    getCurrentUser()
+  }, []);
 
   return (
-    <UserProvider value={{ user, loginUser, logoutuser, updateUser }}>
+    <UserProvider value={{ user, loginUser, logoutUser, updateUser }}>
       <div className="w-4/5 mx-auto">
         <h1 className="text-4xl text-center">ChalChitra</h1>
         {!isLoggedIn && (
--- client/src/components/Logout.jsx
+++ client/src/components/Logout.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Button from './Button'
+import { useUser } from '../contexts'
 
 const Logout = () => {
+  const {logoutUser} = useUser()
+
   return (
     <div>Logout
       <Button
         className={"bg-red-500"}
         type={"submit"}
         textSize={"1.2rem"}
+        onClick={logoutUser}
       >
         Logout
       </Button>
