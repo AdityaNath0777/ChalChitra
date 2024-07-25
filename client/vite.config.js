11915a7f0ccc697b97d62466c467367764f595bd
Aditya_Mishra <adityasaiyansuper@gmail.com>
2024-07-25 12:45:45 +0530

    fix vite config with correct env naming convention

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,14 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
-  const proxy_target = `${env.PROXY}`;
+  const proxy_target = `${env.VITE_PROXY}`;
   return {
     server: {
       proxy: {
         "/api": {
-          target: proxy_target,
+          target:   proxy_target,
           changeOrigin: true,
-          secure: true,
+          secure: false,
         },
       },
     },
