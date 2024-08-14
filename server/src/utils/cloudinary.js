f0b27e3cc9efd74281bfd5f655b73004689a57e1
Aditya_Mishra <adityasaiyansuper@gmail.com>
2024-08-14 22:11:17 +0530

    chore(cloudinary): add utility function to delete file from cloudinary

--- server/src/utils/cloudinary.js
+++ server/src/utils/cloudinary.js
@@ -20,10 +20,9 @@ const uploadOnCloudinary = async (localFilePath) => {
     // file has been uploaded successfully
     // console.log("file has been uploaded on cloudinary", response.url);
     fs.unlinkSync(localFilePath);
-    
+
     // console.log("cloudinary response :: ", response);
     return response;
-
   } catch (error) {
     fs.unlinkSync(localFilePath); // remove the temp. saved file as the upload operation got failed
 
@@ -43,5 +42,25 @@ const uploadOnCloudinary = async (localFilePath) => {
 // // //   // }
 // // // );
 
+const deleteFromCloudinary = async (fileURL, fileType) => {
+  if (!fileType) {
+    fileType = "image";
+  }
+  const publicId = fileURL?.split("/")?.pop()?.split(".")?.[0];
 
-export { uploadOnCloudinary };
+  const options = {
+    resource_type: fileType,
+    type: "upload",
+    invalidate: true,
+  };
+
+  try {
+    const result = await cloudinary.uploader.destroy(publicId, options);
+    console.log("result: ", result);
+    return result;
+  } catch (err) {
+    return null;
+  }
+};
+
+export { uploadOnCloudinary, deleteFromCloudinary };
