0df70f306994291d339006d07e2eaaca80833577
Aditya_Mishra <adityasaiyansuper@gmail.com>
2025-01-01 15:21:38 +0530

    chore(playlist): update playlist model schema

--- server/src/models/playlist.model.js
+++ server/src/models/playlist.model.js
@@ -1,30 +1,36 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const playlistSchema = new Schema(
   {
     name: {
       type: String,
-      required: true
+      required: true,
     },
     description: {
       type: String,
-      required: true
+      required: true,
     },
     owner: {
       type: Schema.Types.ObjectId,
-      ref: "User"
+      ref: "User",
     },
     videos: [
       {
         type: Schema.Types.ObjectId,
-        ref: "Video"
-      }
-    ]
-
+        ref: "Video",
+      },
+    ],
+    isPrivate: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
-    timestamps: true
+    timestamps: true,
   }
-)
+);
 
-export const Playlist = mongoose.model("Playlist", playlistSchema)
+playlistSchema.plugin(mongooseAggregatePaginate);
+
+export const Playlist = mongoose.model("Playlist", playlistSchema);
