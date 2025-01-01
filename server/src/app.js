261f8ea3df4828a65c8fe163e659d7d6636eaad0
Aditya_Mishra <adityasaiyansuper@gmail.com>
2025-01-01 15:35:11 +0530

    feat(api): Implement playlist, like, reply, healthcheck, dashboard API in app.js

--- server/src/app.js
+++ server/src/app.js
@@ -38,6 +38,11 @@ import videoRouter from "./routes/video.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import replyRouter from "./routes/reply.routes.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
 // routes declaration
 // app.get() -> routes and controller hard coded
 
@@ -50,6 +55,16 @@ app.use("/api/v1/subscriptions", subscriptionRouter); // middleware for subscrip
 app.use("/api/v1/comments", commentRouter); // middleware for comment routes
 
 app.use("/api/v1/tweets", tweetRouter); // middleware for tweeet routes
+
+app.use("/api/v1/playlists", playlistRouter); // middleware for playlist routes
+
+app.use("/api/v1/likes", likeRouter); // middleware for like routes
+
+app.use("/api/v1/replies", replyRouter); // middleware for reply routes
+
+app.use("/api/v1/healthcheck", healthcheckRouter); // middleware for healthcheck routes
+
+app.use("/api/v1/dashboard", dashboardRouter); // middleware for dashboard routes
 // http://localhost:portNum/api/v1/users/register
 
 export { app };
