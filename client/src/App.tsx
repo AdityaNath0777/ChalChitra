8f72f186119ff2f651443e78264851b2a22aa777
Aditya_Mishra <adityasaiyansuper@gmail.com>
2025-01-08 02:27:09 +0530

    chore(cleanup): Remove redundant comments in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -52,7 +52,7 @@ function App() {
           <div className="col-span-1 relative">
             <Navbar />
           </div>
-          <div className="col-span-5">
+          <div className="col-span-5 px-8">
             <Routes>
               <Route path="/" element={<Navigate to={"/home"} replace />} />
               <Route path="/home" element={<div>Home</div>} />
@@ -75,49 +75,6 @@ function App() {
           </div>
         </div>
       </Router>
-
-      {/* {!isRegistered && (
-        <div className="w-full mx-auto mt-10">
-          <h1 className="text-4xl text-center main-font">ChalChitra</h1>
-          <div className="w-2/5 mx-auto py-4">
-            <Signin />
-            <Button onClick={() => setIsRegistered(true)}>
-              Already have an account? Login
-            </Button>
-          </div>
-        </div>
-      )}
-      {!isLoggedIn && isRegistered && (
-        <div className="w-full mx-auto mt-10">
-          <h1 className="text-4xl text-center main-font">ChalChitra</h1>
-          <div className="w-2/5 mx-auto py-4">
-            <Login />
-            <Button onClick={() => setIsRegistered(false)}>
-              Don&apos;t have an account? Sign In
-            </Button>
-          </div>
-        </div>
-      )}
-
-      {isLoggedIn && (
-        <div className="my-container grid grid-cols-6 relative">
-          <div className="col-span-1 relative">
-            <Navbar />
-          </div>
-          <div className="col-span-5">
-            {isUpdating ? (
-              <UpdateProfile />
-            ) : (
-              <>
-                <Profile />
-                <Button onClick={() => setIsUpdating(true)}>
-                  Update Account Details
-                </Button>
-              </>
-            )}
-          </div>
-        </div>
-      )} */}
     </UserProvider>
   );
 }
