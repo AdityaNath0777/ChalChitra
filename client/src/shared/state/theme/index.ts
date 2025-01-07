d2950301736ada7cab17ec617804dddc92b7f1d1
Aditya_Mishra <adityasaiyansuper@gmail.com>
2025-01-07 16:56:31 +0530

    feat(theme): Add ThemeContext with light and dark mode support

---
+++ client/src/shared/state/theme/index.ts
@@ -0,0 +1,5 @@
+import { ThemeContext } from "./ThemeContext";
+import { useTheme } from "./useTheme";
+import { ThemeProvider } from "./ThemeProvider";
+
+export { useTheme, ThemeContext, ThemeProvider };
---
+++ client/src/shared/state/theme/themes.ts
@@ -0,0 +1,19 @@
+import { Theme } from "./ThemeContext";
+
+export const lightTheme: Theme = {
+  name: "light",
+  colors: {
+    primary: "#000",
+    secondary: "#fff",
+    background: "#f0f0f0",
+  },
+};
+
+export const darkTheme: Theme = {
+  name: "dark",
+  colors: {
+    primary: "#fff",
+    secondary: "#000",
+    background: "#181818",
+  },
+};
