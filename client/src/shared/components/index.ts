f6016fcf3ea1444eade2c6ec552ee929d51e7e50
Aditya_Mishra <adityasaiyansuper@gmail.com>
2025-01-13 15:01:44 +0530

    chore(components): export desktop and mobile navbars, and header component

--- client/src/shared/components/index.ts
+++ client/src/shared/components/index.ts
@@ -1,6 +1,8 @@
 import Button from "./Button";
-import Header from "./Header";
+import Header from "./header/Header";
 import Navbar from "./Navbar";
 import ScrollToTop from "./ScrollToTop";
+import DesktopNavbar from "./navbar/DesktopNavbar";
+import MobileNavbar from "./navbar/MobileNavbar";
 
-export { Button, Header, Navbar, ScrollToTop };
+export { Button, Header, Navbar, DesktopNavbar, MobileNavbar, ScrollToTop };
