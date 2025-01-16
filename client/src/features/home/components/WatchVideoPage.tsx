9d2da3e02beee557e156858aeddfa3d3211d23c6
Aditya_Mishra <adityasaiyansuper@gmail.com>
2025-01-16 23:59:09 +0530

    feat(video): Add comment section to WatchVideoPage

--- client/src/features/home/components/WatchVideoPage.tsx
+++ client/src/features/home/components/WatchVideoPage.tsx
@@ -2,6 +2,7 @@ import VideoPlayer from "@features/video/components/VideoPlayer";
 import { useSearchParams } from "react-router-dom";
 import "cloudinary-video-player/cld-video-player.min.css";
 import { useVideo } from "@features/video/hooks/VideoContext";
+import CommentSection from "@features/comment/components/CommentSection";
 // import { useEffect } from "react";
 // import { getVideoById } from "@features/video/services/video.services";
 
@@ -32,7 +33,7 @@ const WatchVideoPage = () => {
       </div>
       <div className="bottom-container p-2 w-full col-span-12 md:col-start-2 md:col-span-10 xl:col-start-2 xl:col-span-10 grid grid-cols-12">
         <div className="col-span-12 sm:col-span-8 w-full"></div>
-        {/* Comment section */}
+        <CommentSection videoId={video._id} />
       </div>
     </div>
   );
