856a49e4cec27de418cc7362eb41a1f2122f4a25
Aditya_Mishra <adityasaiyansuper@gmail.com>
2025-01-19 13:48:52 +0530

    add(contexts): Export LayoutProvider and useLayoutContext

--- client/src/shared/contexts/index.ts
+++ client/src/shared/contexts/index.ts
@@ -3,3 +3,5 @@ export { useAuthContext, AuthProvider } from "./auth";
 export { useUserContext, UserProvider } from "./user";
 
 export { useThemeContext, ThemeProvider } from "./theme";
+
+export { useLayoutContext, LayoutProvider } from "./layout";
